74772555d2060cc109000ab057b477ac1357a24b
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-07-31 18:00:43 +0800

    fix: test predict result obtain

--- test_predict.py
+++ test_predict.py
@@ -1,5 +1,5 @@
-from pretrain.exp import Exp_Main
-from pretrain.params import params
+from finetune.exp import Exp_Ft
+from finetune.params import params
 import os
 import json
 
@@ -7,11 +7,14 @@ import json
 if __name__ == '__main__':
     # --data_path ./data/data.bin
     # --index_path ./data/index.bin
-    rslt_path = "./results/rslt.json"
+    rslt_path = "./results/rslt-finetune-10000.json"
     args = params()
+    args.batch_size = 1
+    args.is_train = False
+    args.task = "compensation"
     os.environ['NCCL_ASYNC_ERROR_HANDLING'] = '1'
     local_rank = int(os.environ['LOCAL_RANK'])
-    bartti = Exp_Main(args, local_rank)
+    bartti = Exp_Ft(args, local_rank)
     rslt = bartti.test()
     with open(rslt_path, "w") as f:
         rslt = json.dump(rslt, f)
