f8019d66077ed6c0210eeb48b412fb984b5ee080
Ruyi-Feng <fengruyi@seu.edu.cn>
2024-01-25 22:38:56 +0800

    fix: adjust params

--- pretrain/data_provider.py
+++ pretrain/data_provider.py
@@ -43,8 +43,8 @@ class Data_Form:
             data["width"] = abs(data[cols.right] - data[cols.left])
             data["height"] = abs(data[cols.bottom] - data[cols.top])
         self.min_y = data[cols.top].min() - 10  # 用来把数据集的y规范到较小的范围
-        f_data = open("./data/train/data_val_interval6_len30.bin", 'ab+')
-        f_index = open("./data/train/index_val_interval6_len30.bin", 'ab+')
+        f_data = open("./data/val/data_free_val_interval6_len120.bin", 'ab+')
+        f_index = open("./data/val/index_free_val_interval6_len120.bin", 'ab+')
         self.scale = self.flnms[flnm]["scale"]
         data_s = self._split_data(data, cols)
         data_s = data_s.sort_values(by=["split", cols.frame, cols.car_id]).reset_index(drop=True)
