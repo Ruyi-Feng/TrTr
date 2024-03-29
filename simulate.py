ef98808ed21d36f59830c035e2492205aca7e849
Ruyi-Feng <fengruyi@seu.edu.cn>
2024-03-29 19:31:57 +0800

    fix: params

--- simulate.py
+++ simulate.py
@@ -7,13 +7,13 @@ import json
 if __name__ == '__main__':
     # --data_path ./data/data.bin
     # --index_path ./data/index.bin
-    rslt_path = "./results/histlabel_simu.json"
+    rslt_path = "./results/histseq2seq_simu_epoch48.json"
     args = params()
     args.batch_size = 1
     args.is_train = False
     os.environ['NCCL_ASYNC_ERROR_HANDLING'] = '1'
     local_rank = int(os.environ['LOCAL_RANK'])
     model = Simulator(args, local_rank)
-    rslt = model.run(sample_num=10, simu_len=5)
+    rslt = model.run(sample_num=50, simu_len=3)
     with open(rslt_path, "w") as f:
         rslt = json.dump(rslt, f)
--- test_predict.py
+++ test_predict.py
@@ -7,10 +7,12 @@ import json
 if __name__ == '__main__':
     # --data_path ./data/data.bin
     # --index_path ./data/index.bin
-    rslt_path = "./results/histlabel_local_epoch100.json"
+    rslt_path = "./results/histseq2seq_local_epoch48.json"
     args = params()
     args.batch_size = 1
     args.is_train = False
+    args.data_path = "./data/train/data_val_interval6_len30.bin"
+    args.index_path = "./data/train/index_val_interval6_len30.bin"
     os.environ['NCCL_ASYNC_ERROR_HANDLING'] = '1'
     local_rank = int(os.environ['LOCAL_RANK'])
     model = Exp_Main(args, local_rank)
